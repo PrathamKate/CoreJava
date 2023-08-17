bcee72158356a582ca00cc375c50b2c8245e5112
Pratham Kate <prathamkate7july@gmail.com>
2023-08-17 16:34:21 +0530

    Add files via upload

---
+++ SwitchCases.java
@@ -0,0 +1,40 @@
+package project1;
+
+public class SwitchCases {
+
+	public static void main(String[] args) {
+		int day = 4;
+		switch(day) {
+		case 1:
+			System.out.println("Its Monday Today");
+			break;
+		
+		
+		case 2:
+			System.out.println("Its Tuesday Today");
+			break;
+	
+		case 3:
+			System.out.println("Its Wednesday Today");
+			break;
+
+		case 4:
+			System.out.println("Its Thursday Today");
+			break;
+
+		case 5:
+			System.out.println("Its Friday Today");
+			break;
+
+		case 6:
+			System.out.println("Its Saturday Today");
+			break;
+
+		case 7:
+			System.out.println("Its Monday Today");
+			break;
+		}
+
+	}
+
+}
