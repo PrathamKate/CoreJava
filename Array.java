776e150eb099798531a68394e6a15c0c510f37ae
Pratham Kate <prathamkate7july@gmail.com>
2023-08-21 10:03:19 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,12 @@
+package ProjectJava;
+
+public class Array {
+
+	public static void main(String[] args) {
+		String bikes[]= {"KTM","BMW","Royal Enfield","Honda","Pulsar","Suzuki"};
+		for(int i=0;i<=5;i++) {
+			System.out.println(bikes[i]);
+		}
+	}
+
+}
