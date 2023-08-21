a38d5c331561c347686b1b4d4995000011da6b07
Pratham Kate <prathamkate7july@gmail.com>
2023-08-21 10:55:53 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,18 @@
+package ProjectJava;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		int a=8,b=6;
+		System.out.println("-----Before Swapping-----");
+		System.out.println("The value of A: "+a);
+		System.out.println("The value of B: "+b);
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println("\n------After Swapping-------");
+		System.out.println("The value of A: "+a);
+		System.out.println("The value of B: "+b);
+	}
+
+}
