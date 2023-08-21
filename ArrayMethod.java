3e57d17539817135fbffc1349f511e0631488783
Pratham Kate <prathamkate7july@gmail.com>
2023-08-21 10:25:21 +0530

    Add files via upload

---
+++ ArrayMethod.java
@@ -0,0 +1,13 @@
+package ProjectJava;
+
+public class ArrayMethod {
+
+	public static void main(String[] args) {
+		String bikes[]= {"KTM","BMW","Royal Enfield","Honda","Pulsar","Suzuki"};
+		for(String i:bikes) {
+			System.out.println(i);
+		}
+
+	}
+
+}
