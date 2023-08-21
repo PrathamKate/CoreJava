08daec35d5b76eeeeb43ceb8bd20ea685d12dcaf
Pratham Kate <prathamkate7july@gmail.com>
2023-08-21 09:55:07 +0530

    Add files via upload

---
+++ Break.java
@@ -0,0 +1,16 @@
+package ProjectJava;
+
+public class Break {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++ ) {
+			
+			if(i==4) {
+				break;
+			}
+			System.out.println("i vaule is "+i);
+		}
+		
+	}
+
+}
---
+++ Continue.java
@@ -0,0 +1,16 @@
+package ProjectJava;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++ ) {
+			
+			if(i==4) {
+			continue;
+			}
+			System.out.println("i vaule is "+i);
+		}
+		
+	}
+
+}
