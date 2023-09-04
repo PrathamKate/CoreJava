7edf9019d072cdd2b3589d114c020e3d6ddef4f1
Pratham Kate <prathamkate7july@gmail.com>
2023-09-04 11:28:31 +0530

    Add files via upload

---
+++ WrapperClass.java
@@ -0,0 +1,13 @@
+package com.tns.Wrapper;
+
+public class WrapperClass {
+	
+	public static void main(String[] args) {
+		int a=20;
+		Integer i=a;
+		System.out.println(a);
+		System.out.println(i);
+
+	}
+
+}
---
+++ WrapperObj.java
@@ -0,0 +1,14 @@
+package com.tns.Wrapper;
+
+public class WrapperObj {
+
+	public static void main(String[] args) {
+		Integer a =new Integer(3);
+		int j = a;
+		int i = a .intValue();
+		System.out.println(a);
+		System.out.println(j);
+		System.out.println(i);
+	}
+
+}
